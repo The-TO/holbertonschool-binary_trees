404b6740130a451226aa3921219acc105db0aa8a
The-TO <theovinc@gmail.com>
2026-08-03 15:37:54 +0200

    Add binary_tree_inorder function to traverse a tree in inorder and update header file

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Runs through a tree, preorder way
+ * @tree: Points to the root of the tree
+ * @func: Pointer to the function to call each node
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	binary_tree_preorder(tree->left, *func);
+    func(tree->n);
+	binary_tree_preorder(tree->right, *func);
+}
