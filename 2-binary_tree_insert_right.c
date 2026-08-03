1438fdaffc87592f94d7113cea9f4d740b1e317d
The-TO <theovinc@gmail.com>
2026-08-03 13:44:49 +0200

    Add binary tree insertion function for the right child and update header file

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Function that inserts a node on the left
+ * @parent: Points to the root of the tree
+ * @value: Data stored in the node
+ * Return: new_node (Node inserted)
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *nvx_node;
+
+	if (!parent)
+	{
+		return (NULL);
+	}
+	nvx_node = malloc(sizeof(binary_tree_t));
+	if (!nvx_node)
+	{
+		return (NULL);
+	}
+	nvx_node->n = value;
+	nvx_node->left = NULL;
+	nvx_node->right = NULL;
+	nvx_node->parent = parent;
+
+	if (parent->right != NULL)
+	{
+		nvx_node->right = parent->right;
+		parent->right->parent = nvx_node;
+	}
+	parent->right = nvx_node;
+	return (nvx_node);
+}
