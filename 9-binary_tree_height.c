c8875af7353fc838da06cbfa01740876cd252fbf
The-TO <theovinc@gmail.com>
2026-08-03 16:53:24 +0200

    Fix formatting and indentation in binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,25 +7,25 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (!tree)
-    {
-        return(0);
-    }
-    if (!tree->left && !tree->right)
-    {
-        return (0);
-    }
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right); 
+	if (!tree)
+	{
+		return (0);
+	}
+	if (!tree->left && !tree->right)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    if (left_height > right_height)
-    {
-        return(1 + (left_height));
-    }
-    else
-    {
-        return(1 + (right_height));
-    }
+	if (left_height > right_height)
+	{
+		return (1 + (left_height));
+	}
+	else
+	{
+		return (1 + (right_height));
+	}
 }
