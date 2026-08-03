02db3ee0d17a51eebb77f422360868bf91d62ba6
The-TO <theovinc@gmail.com>
2026-08-03 16:45:51 +0200

    Add binary_tree_height function to calculate the height of a binary tree and update header file

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Function that prints the height of a tree
+ * @tree: Pöints to the beginning of the tree
+ * Return: The height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (!tree)
+    {
+        retrun(0);
+    }
+    if (!tree->left && !tree->right)
+    {
+        return (0);
+    }
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right); 
+
+    if (left_height > right_height)
+    {
+        return(1 + (left_height));
+    }
+    else
+    {
+        return(1 + (right_height));
+    }
+}
