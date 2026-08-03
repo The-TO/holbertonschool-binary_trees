c3fb99dc3408e1e682cdcb25ca1d999256943dfa
The-TO <theovinc@gmail.com>
2026-08-03 15:48:19 +0200

    Add binary_tree_postorder function to traverse a tree in postorder and update header file

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Runs through a tree, preorder way
+ * @tree: Points to the root of the tree
+ * @func: Pointer to the function to call each node
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	binary_tree_postorder(tree->left, *func);
+	func(tree->n);
+	binary_tree_postorder(tree->right, *func);
+}
