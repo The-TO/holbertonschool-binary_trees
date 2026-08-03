8a410b1fec033bb5337a0048482bd8df04e52535
The-TO <theovinc@gmail.com>
2026-08-03 14:04:25 +0200

    Add binary_tree_is_leaf function to check if a node is a leaf and update header file

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ * @node: Node to check if it is a leaf
+ * Return: 1 (if it is a leaf)
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (!node)
+    {
+        return(0);
+    }
+    if (!node->left && !node->right)
+    {
+        return (1);
+    }
+    return(0);
+}
