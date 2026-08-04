cef420f89d9baed19ad01729c926b6e910b00c65
The-TO <theovinc@gmail.com>
2026-08-04 14:27:19 +0200

    Add binary_tree_is_perfect function to check if a binary tree is perfect and update header file

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - Function that checks if a tree is perfect
+ * @tree: points to the tree
+ * Return: if it is perfect
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t hauteur, taille;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	hauteur = binary_tree_height(tree);
+	taille = binary_tree_size(tree);
+
+	return (taille == (1 << (hauteur + 1)) - 1);
+}
+
